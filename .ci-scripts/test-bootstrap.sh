5b324dcabcd252142ad93bc82d7890814e691f38
Sean T. Allen <sean@seantallen.com>
2020-08-29 10:25:10 -0400

    Add bootstrap test on FreeBSD

--- .ci-scripts/test-bootstrap.sh
+++ .ci-scripts/test-bootstrap.sh
@@ -2,11 +2,13 @@
 
 ./ponyup-init.sh --repository=nightlies
 
+MAKE=${MAKE:=make}
+
 export PATH=$HOME/.local/share/ponyup/bin:$PATH
 ponyup update ponyc nightly
 ponyup update changelog-tool nightly
 ponyup update corral nightly
 ponyup update stable nightly
 
-make clean
-make
+${MAKE} clean
+${MAKE}
